07c338a03fa1eda72530600cac0065a9d505bc20
alumno <alumno@utnfra.com.ar>
2020-02-29 12:33:20 -0300

    sabado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,42 @@
+/*b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+c
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva */
+
 function mostrar()
 {
+    var nombre;
+    var cantidadPersonas;
+    var cantidadDias;
+    var formadepago;
+    var respuesta=true;
+    var contador;
+    var acumulador;
+
+while (respuesta){
+
+    do{
+    nombre = prompt("Ingrese un nombre");
+
+    }while(!isNaN(nombre));
+
+    do{
+    cantidadPersonas = prompt("Ingrese la cantidad de personas");
+    cantidadPersonas = parseInt(cantidadPersonas);
+    }while(isNaN(cantidadPersonas) || cantidadPersonas<1);
+    
+    do{
+    cantidadDias = prompt("Ingrese la cantidad de dias de estadia");
+    cantidadDias = parseInt(cantidadDias);
+    }while(isNaN(cantidadDias));
+
+}
 
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+    var nombre;
+    var edad;
+    var sexo;
+    var respuesta=true;
+    var nota;
+    var contador;
+    var acumulador;
+
+
+    while (respuesta){
+        do{
+            nombre = prompt("Ingrese un nombre");
+        }while(!isNaN(nombre));
+
+        do{
+            edad = prompt("Ingrese una edad");
+            edad = parseInt(edad);
+        }while(isNaN(edad));
+
+        do{
+            sexo = prompt("Ingrese el sexo f o m");
+        
+        }while(!isNaN(sexo) || sexo != "m" && sexo != "f");
+}
 
 }
