dc897578e85ff01b0511aff2cf71cec50609c0a0
Eugenia <eugeperonace@gmail.com>
2020-03-08 22:54:58 -0300

    ejercicio 2 corregido

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,98 @@
 function mostrar()
+
+/*Realizar un algoritmo que permita ingresar los datos de una compra de ingredientes para preparar comida al 
+por mayor, hasta que el cliente quiera:
+peso (entre 10 y 1000) en kilo,
+precio por kilo (mas de cero),
+tipo validad ("v","a","m")(vegetal, animal, mezcla)
+
+Si compro mas de 100 kilos en total tenes 15% de descuento sobre el total a pagar.
+Si compro mas de 300 en total tenes 25% de descuento sobre el total a pagar.
+
+a) el importe total a pagar, bruto sin descuento
+b) el importe total a pagar con descuento (si corresponde)
+c) informar el tipo de alimento mas caro
+d) el promedi de precio por kilo en total
+
+*/
+
 {
+    var peso;
+    var precio;
+    var tipo;
+    var respuesta = true;
+    var primeraVez = true;
+    var acumPeso = 0;
+    var acumPrecioParcial = 0;
+    var precioTotal;
+    var precioConDescuento;
+    var precioAlimentoMasCaro;
+    var tipoAlimentoMasCaro;
+    var acumPrecioDeCadaUno = 0;
+    var contadorPrecios = 0;
+
+    while(respuesta){
+    
+    do{
+        peso = prompt("INGRESE PESO");
+        peso = parseInt(peso);
+
+    }while (isNaN(peso) || peso <=9 || peso >1000);
+    
+    do{
+        precio = prompt("INGRESE PRECIO");
+        precio = parseInt(precio);
+
+    }while (isNaN(precio) || precio <=0);
+
+    do{
+        tipo = prompt("INGRESE TIPO v, a, m ");
+
+	}while (!isNaN(tipo) || tipo != "v" && tipo != "a" && tipo != "m");
+
+
+    //a) el importe total a pagar, bruto sin descuento
+
+    acumPeso += peso;
+    acumPrecioParcial += precio;
+    precioTotal = acumPrecioParcial * acumPeso;
+
+    respuesta = confirm("Desea seguir ingresando datos?");
+
+    //b) el importe total a pagar con descuento (si corresponde)
+
+    if ( peso > 100){
+     precioConDescuento = 85*precioTotal /100;
+
+	}else{
+        if ( peso > 300){
+        precioConDescuento = 75*precioTotal /100; 
+		}
+	}
+
+    // c) informar el tipo de alimento mas caro
+
+    if (primeraVez || precio > precioAlimentoMasCaro){
+        primeraVez = false;
+        precioAlimentoMasCaro = precio;
+        tipoAlimentoMasCaro = tipo;
+	}
+
+    // d) el promedio de precio por kilo en total
+
+    acumPrecioDeCadaUno += precio;
+    contadorPrecios++;
+
+    
+
+	} // fin while
   
-}
+  promedioPrecio = acumPrecioDeCadaUno / contadorPrecios;
+
+  document.write ("El importe total a pagar sin descuento es: " +precioTotal+ "<br>");
+  document.write ("El importe total a pagar con descuento es de: " +precioConDescuento+ "<br>");
+  document.write ("El tipo de alimento mas caro es: " +tipoAlimentoMasCaro+ "<br>");
+  document.write ("El promediod e precio por kilo en todal es de: " +promedioPrecio+ "<br>");
+
+
+} // fin function
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,113 @@
 function mostrar()
-{
 
-}
+/*
+En el ingreso a un viaje en crucero nos solicitan nombre, edad (mayores de 18), sexo("f" o "m") 
+y estado civil ("soltero", "casado" o "viudo");
+a) El nombre del hombre casado más joven
+b) El sexo y nombre del pasajero más viejo
+c) La cantidad de mujeres que hay casadas o viudas
+d) El promedio de edad entre las mujeres
+e) El promedio de edad entre los hombres solteros
+*/
+{
+	var nombre;
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var respuesta = true;
+	var primeraVez = true;
+	var edadCasadoMasJoven;
+	var nombreCasadoMasJoven;
+	var edadMasViejo;
+	var nombreMasViejo;
+	var sexoMasViejo;
+	var contadorMujeres = 0;
+	var acumuladorEdadMujeres = 0;
+	var acumuladorEdadHombresSolteros = 0;
+	var contadorHombresSolteros = 0;
+	var promedioEdadMujeres;
+	var promedioEdadHombresSolteros;
+
+	while(respuesta){
+	
+	do{
+		nombre = prompt("INGRESE SU NOMBRE");
+
+	}while (!isNaN(nombre));
+
+	do{
+		edad = prompt("INGRESE SU EDAD");
+		edad = parseInt(edad);
+
+	}while (isNaN(edad) || edad <=17);
+
+	do{
+		sexo = prompt("INGRESE SU SEXO");
+
+	}while (!isNaN(sexo) || sexo != "f" && sexo != "m");
+
+	do{
+		estadoCivil = prompt("INGRESE SU ESTADO CIVIL (soltero, casado, viudo)");
+
+	}while (!isNaN(estadoCivil) || estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo");
+
+
+	//a) El nombre del hombre casado mas joven
+
+	if (sexo == "m" && primeraVez || estadoCivil == "casado" || edad > edadCasadoMasJoven){
+		nombreCasadoMasJoven = nombre;
+		edadCasadoMasJoven = edad;
+	}
+
+	//b) El sexo y nombre del pasajero más viejo
+
+	if (primeraVez || edad > edadMasViejo){
+		edadMasViejo = edad;
+		nombreMasViejo = nombre;
+		sexoMasViejo = sexo;
+	}
+
+	//c) La cantidad de mujeres que hay casadas o viudas
+
+	if (sexo == "m" && primeraVez|| estadoCivil == "casado" || estadoCivil == "viudo" ){
+		contadorMujeres++;
+	}
+
+	//d) El promedio de edad entre las mujeres
+
+	if (sexo == "f"){
+	acumuladorEdadMujeres += edad;
+	contadorMujeres++;
+
+	//e) El promedio de edad entre los hombres solteros
+
+	}else{
+		if (sexo == "m" || estadoCivil == "soltero"){
+			acumuladorEdadHombresSolteros += edad;
+			contadorHombresSolteros++;
+		}
+	}
+	
+	//Continuar
+	primeraVez = false;
+	respuesta = confirm("Desea seguir ingresando datos?");
+
+
+	} // fin while
+
+	promedioEdadMujeres = acumuladorEdadMujeres / contadorMujeres;
+
+	promedioEdadHombresSolteros = acumuladorEdadHombresSolteros / contadorHombresSolteros;
+
+
+	document.write ("El nombre del hombre casado mas joven es: " +nombreCasadoMasJoven+ "<br>" );
+	document.write ("El sexo del pasajero mas viejo es: " +sexoMasViejo+ " y su nombre es: " +nombreMasViejo+ "<br>" );
+	document.write ("La cantidad de mujeres casadas o viudas es de: " +contadorMujeres+ "<br>");
+	document.write ("El promedio de edad entre las mujeres es de: " +promedioEdadMujeres+ "<br>");
+	document.write ("El promedio de edad entre los hombres solteros es de: " +promedioEdadHombresSolteros+ "<br>");
+
+
+
+
+} // fin function 
+
